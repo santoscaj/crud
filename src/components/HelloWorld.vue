557e759c4520e4150af76d18527266bdd9f52b85
bertech <santosc.aj@gmail.com>
2020-04-01 06:15:00 -0300

    checking

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="hello">
-   
+      <Sidebar/>
+      <Data/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Sidebar from '@/components/Sidebar.vue'
+import Data from '@/components/Data.vue'
 
 export default Vue.extend({
   name: 'HelloWorld',
+  components: {
+    Sidebar,
+    Data
+  },
   props: {
     msg: String,
   },
@@ -17,6 +24,12 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello{
+  width: 100%;
+  height: 100%;
+  display:flex;  
+}
+
 h3 {
   margin: 40px 0 0;
 }
