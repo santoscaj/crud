0bc49880784e85bd0ed597056b7bf32a2d012178
bertech <santosc.aj@gmail.com>
2020-04-01 06:15:25 -0300

    checking again... missed two components

---
+++ src/components/Data.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    Hwllo2
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html,*{
+  background: bisque;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
